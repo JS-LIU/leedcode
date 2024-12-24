f7490e412eb671109eae3eece7eecccaff5b3086
JS-LIU <984003917@qq.com>
2024-12-25 00:28:31 +0800

    feat:199 rightsideview

--- 199.二叉树的右视图.js
+++ 199.二叉树的右视图.js
@@ -18,7 +18,22 @@
  * @return {number[]}
  */
 var rightSideView = function(root) {
-    
+    let deepth = 0;
+    let res = [];
+    function tranverse(root,deepth){
+        if(!root){
+            return;
+        }
+        deepth++;
+        if(res.length < deepth ){
+            res.push(root.val);
+        }
+        tranverse(root.right,deepth);
+        tranverse(root.left,deepth);
+        deepth--;
+    }
+    tranverse(root,deepth);
+    return res;
 };
 // @lc code=end
 
